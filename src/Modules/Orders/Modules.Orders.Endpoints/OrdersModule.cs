4d142ef716e9fc61dc39e24672cdf0166e380f16
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2024-01-02 10:03:52 +1000

    Added Orders API

---
+++ src/Modules/Orders/Modules.Orders.Endpoints/OrdersModule.cs
@@ -0,0 +1,26 @@
+﻿namespace Modules.Orders.Endpoints;
+
+public static class OrdersModule
+{
+    public static void AddOrdersServices(this IServiceCollection services)
+    {
+    }
+
+    public static void UseOrdersModule(this WebApplication app)
+    {
+        app.MapGet("/api/orders", () =>
+            {
+                var orders = Enumerable.Range(1, 5).Select(index => new OrderDto
+                (
+                    Name: $"Order {index}",
+                    Description: $"Order {index} description"
+                ));
+
+                return orders;
+            })
+            .WithName("GetWeatherForecast")
+            .WithOpenApi();
+    }
+}
+
+record OrderDto(string Name, string Description);
