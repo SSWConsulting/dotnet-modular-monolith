2435c96742046c216368cc5e86a1f66dc3859c1f
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2024-07-31 18:01:31 +1000

    ♻️ Make Money more resilient

--- src/Common/Common.SharedKernel/Domain/Entities/Money.cs
+++ src/Common/Common.SharedKernel/Domain/Entities/Money.cs
@@ -1,4 +1,6 @@
-﻿namespace Common.SharedKernel.Domain.Entities;
+﻿using Throw;
+
+namespace Common.SharedKernel.Domain.Entities;
 
 public record Money(Currency Currency, decimal Amount)
 {
@@ -6,15 +8,45 @@ public record Money(Currency Currency, decimal Amount)
 
     public static Money Zero => Default;
 
-    public static Money operator +(Money left, Money right) => new Money(left.Currency, left.Amount + right.Amount);
+    public static Money operator +(Money left, Money right)
+    {
+        AssertValidCurrencies(left, right);
+        return left with { Amount = left.Amount + right.Amount };
+    }
 
-    public static Money operator -(Money left, Money right) => new Money(left.Currency, left.Amount - right.Amount);
+    public static Money operator -(Money left, Money right)
+    {
+        AssertValidCurrencies(left, right);
+        return left with { Amount = left.Amount - right.Amount };
+    }
 
-    public static bool operator <(Money left, Money right) => left.Amount < right.Amount;
+    public static bool operator <(Money left, Money right)
+    {
+        AssertValidCurrencies(left, right);
+        return left.Amount < right.Amount;
+    }
 
-    public static bool operator <=(Money left, Money right) => left.Amount <= right.Amount;
+    public static bool operator <=(Money left, Money right)
+    {
+        AssertValidCurrencies(left, right);
+        return left.Amount <= right.Amount;
+    }
 
-    public static bool operator >(Money left, Money right) => left.Amount > right.Amount;
+    public static bool operator >(Money left, Money right)
+    {
+        return left.Amount > right.Amount;
+    }
 
-    public static bool operator >=(Money left, Money right) => left.Amount >= right.Amount;
+    public static bool operator >=(Money left, Money right)
+    {
+        return left.Amount >= right.Amount;
+    }
+
+    public static Money operator *(Money left, Money right)
+    {
+        AssertValidCurrencies(left, right);
+        return left with { Amount = left.Amount * right.Amount };
+    }
+
+    private static void AssertValidCurrencies(Money left, Money right) => left.Throw().IfNotEquals(right);
 }
