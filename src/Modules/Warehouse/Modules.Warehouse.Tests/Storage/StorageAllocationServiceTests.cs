f9ff7224b007af56d34cc3b163891968edfb168a
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2024-08-26 21:08:07 +1000

    🧪 Consolidate StorageAllocationServiceTests files
    Removed redundant StorageAllocationServiceTests.cs and merged its contents into the existing Storage/StorageAllocationServiceTests.cs. This cleanup reduces duplication and improves file organization in test modules.
    
    #32

--- src/Modules/Warehouse/Modules.Warehouse.Tests/Storage/StorageAllocationServiceTests.cs
+++ src/Modules/Warehouse/Modules.Warehouse.Tests/Storage/StorageAllocationServiceTests.cs
@@ -2,45 +2,88 @@ using FluentAssertions;
 using Modules.Warehouse.Products.Domain;
 using Modules.Warehouse.Storage.Domain;
 
-namespace Modules.Warehouse.Tests.Storage
+namespace Modules.Warehouse.Tests.Storage;
+
+public class StorageAllocationServiceTests
 {
-    public class StorageAllocationServiceTests
+    [Fact]
+    public void AllocateStorage_ShouldAssignProductToFirstEmptyShelf()
     {
-        [Fact]
-        public void AllocateStorage_ShouldAssignProductToFirstEmptyShelf()
+        // Arrange
+        var productId = new ProductId(Guid.NewGuid());
+        var aisles = new List<Aisle>
         {
-            // Arrange
-            var productId = new ProductId(Guid.NewGuid());
-            var aisles = new List<Aisle>
-            {
-                Aisle.Create("name", 2, 2)
-            };
+            Aisle.Create("name", 2, 2)
+        };
 
-            // Act
-            var result = StorageAllocationService.AllocateStorage(aisles, productId);
+        // Act
+        var result = StorageAllocationService.AllocateStorage(aisles, productId);
 
-            // Assert
-            result.IsError.Should().BeFalse();
-            aisles[0].Bays[0].Shelves[0].ProductId.Should().Be(productId);
+        // Assert
+        result.IsError.Should().BeFalse();
+        aisles[0].Bays[0].Shelves[0].ProductId.Should().Be(productId);
+    }
+
+    [Fact]
+    public void AllocateStorage_ShouldThrowException_WhenNoEmptyShelfIsAvailable()
+    {
+        // Arrange
+        var productId = new ProductId(Guid.NewGuid());
+        var aisles = new List<Aisle>
+        {
+            Aisle.Create("name", 1, 1)
+        };
+        StorageAllocationService.AllocateStorage(aisles, productId);
+
+        // Act
+        var result = StorageAllocationService.AllocateStorage(aisles, productId);
+
+        // Assert
+        result.IsError.Should().BeTrue();
+        result.FirstError.Should().Be(StorageAllocationErrors.NoAvailableStorage);
+    }
+
+
+    [Fact]
+    public void AllocateStorage_WhenMaxStorageUsed_ShouldHaveNoAvailableStorage()
+    {
+        // Arrange
+        var numBays = 2;
+        var numShelves = 3;
+        var aisle = Aisle.Create("Aisle 1", numBays, numShelves);
+        var sut = new StorageAllocationService();
+        var productId = new ProductId(Guid.NewGuid());
+
+        // Act
+        for (var i = 0; i < numBays * numShelves; i++)
+        {
+            StorageAllocationService.AllocateStorage([aisle], productId);
         }
 
-        [Fact]
-        public void AllocateStorage_ShouldThrowException_WhenNoEmptyShelfIsAvailable()
+        // Assert
+        aisle.TotalStorage.Should().Be(numBays * numShelves);
+        aisle.AvailableStorage.Should().Be(0);
+    }
+
+
+    [Fact]
+    public void AllocateStorage_ShouldThrowException_WhenNoEmptyShelf()
+    {
+        // Arrange
+        var numBays = 2;
+        var numShelves = 3;
+        var aisle = Aisle.Create("Aisle 1", numBays, numShelves);
+        var sut = new StorageAllocationService();
+        var productId = new ProductId(Guid.NewGuid());
+
+        // Act
+        for(var i = 0; i < numBays * numShelves; i++)
         {
-            // Arrange
-            var productId = new ProductId(Guid.NewGuid());
-            var aisles = new List<Aisle>
-            {
-                Aisle.Create("name", 1, 1)
-            };
-            StorageAllocationService.AllocateStorage(aisles, productId);
-
-            // Act
-            var result = StorageAllocationService.AllocateStorage(aisles, productId);
-
-            // Assert
-            result.IsError.Should().BeTrue();
-            result.FirstError.Should().Be(StorageAllocationErrors.NoAvailableStorage);
+            StorageAllocationService.AllocateStorage([aisle], productId);
         }
+
+        // Assert
+        var result = StorageAllocationService.AllocateStorage([aisle], productId);
+        result.IsError.Should().BeTrue();
     }
 }
