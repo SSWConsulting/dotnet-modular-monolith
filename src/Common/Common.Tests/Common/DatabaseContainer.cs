382af55faa3eec8ae4294557a42a4ad05d3ec1ac
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2024-09-25 20:29:47 +1000

    🧪 added retry to test container creation

--- src/Common/Common.Tests/Common/DatabaseContainer.cs
+++ src/Common/Common.Tests/Common/DatabaseContainer.cs
@@ -15,12 +15,42 @@ public class DatabaseContainer
         .WithAutoRemove(true)
         .Build();
 
+    private const int MaxRetries = 5;
+
     public string? ConnectionString { get; private set; }
 
     public async Task InitializeAsync()
     {
-        await _container.StartAsync();
+        await StartWithRetry();
         ConnectionString = _container.GetConnectionString();
+    }
+
+    private async Task StartWithRetry()
+    {
+        // NOTE: For some reason the container sometimes fails to start up.  Add in a retry to protect against this
+        var notReady = true;
+        var numTries = 0;
+
+        while (notReady)
+        {
+            if (numTries >= MaxRetries)
+            {
+                Console.WriteLine("Max tries reached, giving up");
+                break;
+            }
+
+            try
+            {
+                await _container.StartAsync();
+                notReady = false;
+            }
+            catch (Exception ex)
+            {
+                numTries++;
+                await Task.Delay(2000);
+                Console.WriteLine($"container failed to start: {ex.Message}");
+            }
+        }
     }
 
     public async Task DisposeAsync()
